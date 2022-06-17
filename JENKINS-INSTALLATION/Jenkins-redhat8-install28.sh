27ff9ee498dcffe9cb07b967504ea58720a26964
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2022-06-16 20:18:15 -0400

    Create Jenkins-redhat8-install28.sh

---
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -0,0 +1,20 @@
+# CREATE HOSTNAME
+sudo hostname auto
+sudo yum install wget -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+# start jenkins
+# Start Jenkins
+# You can enable the Jenkins service to start at boot with the command:
+sudo systemctl enable jenkins
+#You can start the Jenkins service with the command:=
+sudo systemctl start jenkins
+# You can check the status of the Jenkins service using the command:
+sudo systemctl status jenkins
+sudo su - ec2-user
