f2a5225d952a5ada552d0fca88d4746464064ab1
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2022-12-03 12:07:48 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-# Use this script to install tomcat in rehat servers
-echo delete the failed version of tomcat
-sudo rm -rf /opt/tomcat9
-echo assign a hostname to your server 
-sudo hostname tomcat
+# TOMCAT.SH
+# Steps for Installing tomcat9 on rhel7&8
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
+# https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
+sudo hostnamectl set-hostname tomcat
 cd /opt 
 sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
-sudo yum install wget unzip -y
-
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.64/bin/apache-tomcat-9.0.64.tar.gz
-sudo tar -xvf apache-tomcat-9.0.64.tar.gz
-sudo rm apache-tomcat-9.0.64.tar.gz
-sudo mv apache-tomcat-9.0.64 tomcat9
+# dowanload and extract tomcat software
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
+sudo tar -xvf apache-tomcat-9.0.69.tar.gz
+sudo rm apache-tomcat-9.0.69.tar.gz
 sudo chmod 777 -R /opt/tomcat9
-sudo chown ec2-user -R /opt/tomcat9
-sh /opt/tomcat9/bin/startup.sh
-# create a soft link to start and stop tomcat
+sudo sh /opt/tomcat9/bin/startup.sh
+# create a soft link to start and stop tomcat from anywhere 
 sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
 sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
-sudo yum update -y
-starttomcat
+sudo starttomcat
 echo "end on tomcat installation"
 #========
 
