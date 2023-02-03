736c7bb145caece14f60d20ce60daa4a161a7fbc
Jazzmangit <118638605+Jazzmangit@users.noreply.github.com>
2023-02-03 17:07:45 +0000

    Create k8s_Manager.sh

---
+++ kubeadm/k8s_Manager.sh
@@ -0,0 +1,11 @@
+sudo hostnamectl set-hostname manager
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+# and then append (or prepend) ~/.local/bin to $PATH
+kubectl version --client
+
+sudo su -
