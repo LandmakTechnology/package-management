90e37810545a81d6fe107c89ed98c284a7f864a8
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2023-12-15 09:49:14 -0800

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -9,12 +9,12 @@ sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
 # dowanload and extract tomcat software
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz
-sudo tar -xvf apache-tomcat-9.0.71.tar.gz
-sudo mv apache-tomcat-9.0.71 tomcat9
-sudo rm apache-tomcat-9.0.69.tar.gz
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
+sudo tar -xvf apache-tomcat-9.0.83.tar.gz
+sudo rm -rf apache-tomcat-9.0.83.tar.gz
+sudo mv apache-tomcat-9.0.83 tomcat9
 sudo chmod 777 -R /opt/tomcat9
-sudo sh /opt/tomcat9/bin/startup.sh
+
 # create a soft link to start and stop tomcat from anywhere 
 # This will enable you to manage tomcat as a service
 sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
