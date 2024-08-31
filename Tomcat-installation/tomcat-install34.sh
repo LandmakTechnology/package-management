8c36976627bcd01fbf919b1887c1c2e2c50493fe
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2024-08-31 14:00:23 -0400

    Update tomcat-install34.sh

--- Tomcat-installation/tomcat-install34.sh
+++ Tomcat-installation/tomcat-install34.sh
@@ -9,11 +9,11 @@ sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.86/bin/apache-tomcat-9.0.86.zip
-sudo unzip apache-tomcat-9.0.86.zip
-sudo rm -rf apache-tomcat-9.0.86.zip
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.93/bin/apache-tomcat-9.0.93.zip
+sudo unzip apache-tomcat-9.0.93.zip
+sudo rm -rf apache-tomcat-9.0.93.zip
 ### rename tomcat for good naming convention
-sudo mv apache-tomcat-9.0.86 tomcat9
+sudo mv apache-tomcat-9.0.93 tomcat9
 ### assign executable permissions to the tomcat home directory
 sudo chmod 775 -R /opt/tomcat9
 sudo chown ec2-user -R /opt/tomcat9
