dab1ea277e495dcc50a7a3f397d8a8a4caa694eb
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2024-12-22 16:37:30 -0500

    Update jenkins-install38.sh

--- JENKINS-INSTALLATION/jenkins-install38.sh
+++ JENKINS-INSTALLATION/jenkins-install38.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #create a T2-MEDIUM REDHAT INSTANCE IN AWS and install Jenkins
+# The scripts/commands that follows will have jenkins installed
 sudo hostnamectl set-hostname auto
-sudo yum -y install unzip wget tree git -y    
+sudo yum -y install unzip wget tree git -y
 # Add required dependencies for the jenkins package
 sudo yum install fontconfig java-17-openjdk -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-sudo yum upgrade -y 
-sudo yum install jenkins -y  
+sudo yum upgrade -y
+sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+sudo su - ec2-user
