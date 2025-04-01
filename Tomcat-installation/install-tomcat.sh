55de39d31967e61985d116f4e4a65449e499055c
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2025-03-31 20:14:02 -0400

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -2,20 +2,22 @@
 # TOMCAT.SH
 # Steps for Installing tomcat9 on rhel7&8
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
-# https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
 
-# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
+#!/bin/sh
+# change hostname to tomcat
 sudo hostnamectl set-hostname tomcat
 cd /opt 
+# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
-sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz 
-sudo tar -xvf apache-tomcat-9.0.80.tar.gz
-sudo rm -rf apache-tomcat-9.0.80.tar.gz
+
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.0.102.zip
+sudo unzip apache-tomcat-9.0.102.zip
+sudo rm -rf apache-tomcat-9.0.102.zip
 ### rename tomcat for good naming convention
-sudo mv apache-tomcat-9.0.80 tomcat9
+sudo mv apache-tomcat-9.0.102 tomcat9 
 ### assign executable permissions to the tomcat home directory
 sudo chmod 777 -R /opt/tomcat9
 sudo chown ec2-user -R /opt/tomcat9
@@ -26,27 +28,9 @@ sh /opt/tomcat9/bin/startup.sh
 sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
 sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
 starttomcat
+sudo su - ec2-user
 echo "end on tomcat installation"
 #========
 
-#2. Start of tomcat configuration 
 
-#Tomcat server configuration:
-find / -name server.xml context.xml
-vim /opt/tomcat9/conf/server.xml
-vi /opt/tomcat9/webapps/manager/META-INF/context.xml
-vi /opt/tomcat9/conf/tomcat-user.xml  # to add user
-
-	<user username="landmark" password="admin" roles="manager-gui,admin-gui"/>
-	
-
-/opt/tomcat9/conf/context.xml
-
- vi /opt/tomcat9/webapps/manager/META-INF/context.xml
-  
-  vi /opt/tomcat9/conf/tomcat-user.xml  # to add user
-  
-	
-	username YourName password=PassWord   roles=manager-gui
-	
 	
