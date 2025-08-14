ecf1d31093fab8a07a689559e5b21435689760c2
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2025-08-14 19:35:38 -0400

    Update nexus-install-augu2025.sh

--- nexus-installation/nexus-install-augu2025.sh
+++ nexus-installation/nexus-install-augu2025.sh
@@ -7,11 +7,11 @@ sudo hostnamectl set-hostname nexus
 sudo useradd nexus
 # Grand sudo access to nexus user
 sudo echo "nexus ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nexus
-sudo su - nexus
 
 ## 3. Enable PasswordAuthentication
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart 
+sudo su - nexus
 
 #4. Install wget
 sudo yum install wget tree vim nano zip unzip -y
